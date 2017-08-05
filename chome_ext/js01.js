9ddec0a17323d95b23097004696322c0abeea32b
jackdizhu <376365334@qq.com>
2017-08-05 21:11:36 +0800

    .ext

--- chome_ext/js01.js
+++ chome_ext/js01.js
@@ -1,7 +1,17 @@
+
 // 02
 (function() {
     'use strict';
     console.log('::chome_ext');
+    var _time;
+    function is_empty(_this) {
+        var ht = _this.innerHTML;
+        if(ht != ''){
+            index();
+        }else{
+            clearTimeout(_time);
+        }
+    }
     function index() {
         var u = window.location.href;
         var U = {
@@ -10,21 +20,24 @@
                 id:['content_right']
             }
         };
-        var R,k;
+        var R,k,_this;
         for(k in U){
             R = new RegExp("^http[s]?:\/\/[a-z.]+"+U[k].R+"");
             if(R.test(u)){
                 for(var i = 0;i<U[k].id.length;i++){
                     if(document.getElementById(U[k].id[i])){
-                        document.getElementById(U[k].id[i]).style.display = 'none';
-                        // console.log('--'+U[k].id[i]+'--');
+                        // document.getElementById(U[k].id[i]).style.display = 'none';
+                        console.log('--:#'+U[k].id[i]+'--');
+                        _this = document.getElementById(U[k].id[i]);
+                        _this.innerHTML = '';
+                        _time = setTimeout(function () {
+                            is_empty(_this);
+                        },3000);
                     }
                 }
             }
         }
     }
-    setInterval(function () {
-        index();
-    },3000);
+    setTimeout(index(),0);
 
 })();
