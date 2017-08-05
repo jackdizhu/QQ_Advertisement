542a5ac07ec4d26401518d2aba8484c7ca5e94d1
jackdizhu <376365334@qq.com>
2017-08-05 20:41:09 +0800

    .ext

--- chome_ext/js01.js
+++ chome_ext/js01.js
@@ -1,23 +1,30 @@
 // 02
 (function() {
     'use strict';
-    var u = window.location.href;
-    var U = {
-        baidu:{
-            R: 'baidu.com',
-            id:['content_right']
-        }
-    };
-    var R,k;
-    for(k in U){
-        R = new RegExp("^http[s]?:\/\/[a-z.]+"+U[k].R+"");
-        if(R.test(u)){
-            for(var i = 0;i<U[k].id.length;i++){
-                if(document.getElementById(U[k].id[i])){
-                    document.getElementById(U[k].id[i]).style.display = 'none';
+    console.log('::chome_ext');
+    function index() {
+        var u = window.location.href;
+        var U = {
+            baidu:{
+                R: 'baidu.com',
+                id:['content_right']
+            }
+        };
+        var R,k;
+        for(k in U){
+            R = new RegExp("^http[s]?:\/\/[a-z.]+"+U[k].R+"");
+            if(R.test(u)){
+                for(var i = 0;i<U[k].id.length;i++){
+                    if(document.getElementById(U[k].id[i])){
+                        document.getElementById(U[k].id[i]).style.display = 'none';
+                        // console.log('--'+U[k].id[i]+'--');
+                    }
                 }
             }
         }
     }
+    setInterval(function () {
+        index();
+    },3000);
 
 })();
