595195c3ffde982f754f3a66bebb5101c86e7077
jackdizhu <376365334@qq.com>
2017-08-05 18:22:16 +0800

    Update _adDisplayNone.js

--- 浏览器_html 广告处理/_adDisplayNone.js
+++ 浏览器_html 广告处理/_adDisplayNone.js
@@ -19,3 +19,27 @@
     }
 
 })();
+// 02
+(function() {
+    'use strict';
+    var u = window.location.href;
+    var U = {
+        baidu:{
+            R: 'baidu.com',
+            id:['content_right']
+        }
+    };
+    var R;
+
+    var k;
+    for(k in U){
+        R = new RegExp("^http[s]?:\/\/[a-z.]+"+U[k].R+"");
+        console.log(R);
+        if(R.test(u)){
+            for(var i = 0;i<U[k].id.length;i++){
+                document.getElementById(U[k].id[i]).style.display = 'none';
+            }
+        }
+    }
+    
+})();
