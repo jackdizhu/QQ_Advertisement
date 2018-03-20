b20bbfd587ba0b4d76bb179c1b42fd929f46c3ed
jackdizhu <376365334@qq.com>
2018-03-20 16:16:49 +0800

    cdn 路径修改

--- chome_ext/js01.js
+++ chome_ext/js01.js
@@ -6,7 +6,7 @@
     var _script = document.createElement('script');
     _script.type = 'text/javascript';
     _script.id = '_chome_ext';
-    _script.src = 'https://jackdizhu.github.io/html_demo/cdn/chome_ext.js';
+    _script.src = 'https://jackdizhu.github.io/advertisement/chome_ext/chome_ext.js';
     _head.appendChild(_script);
 
 })();
