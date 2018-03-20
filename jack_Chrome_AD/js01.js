9b2bc2cc8c3537214a8c312f0a252061abe73d5d
jackdizhu <376365334@qq.com>
2018-03-20 18:50:39 +0800

    js

--- jack_Chrome_AD/js01.js
+++ jack_Chrome_AD/js01.js
@@ -10,7 +10,7 @@
     var _script = document.createElement('script');
     _script.type = 'text/javascript';
     _script.id = '_chome_ext';
-    _script.src = 'https://jackdizhu.github.io/advertisement/chome_ext/chome_ext.js';
+    _script.src = 'https://jackdizhu.github.io/advertisement/chome_ext/chome_ext.js?' + new Date().getTime();
     _head.appendChild(_script);
 
 })();
