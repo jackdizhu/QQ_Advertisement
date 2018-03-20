2b63105010c3e0b66485d13b6fd46b75a3674d25
jackdizhu <376365334@qq.com>
2018-03-20 17:58:18 +0800

    crx 文件打包

--- build_crx.js
+++ build_crx.js
@@ -1,15 +1,16 @@
 const fs = require("fs")
-const ChromeExtension = require("crx")
+var gulp = require('gulp')
+var crx = require('gulp-crx-pack')
 
-const crx = new ChromeExtension({
-  codebase: "http://localhost:8000/jack_Chrome_AD.crx",
-  privateKey: fs.readFileSync("./jack_Chrome_AD_crx/jack_Chrome_AD.pem")
+var manifest = require('./jack_Chrome_AD/manifest.json')
+
+gulp.task('crx', function() {
+  return gulp.src('./jack_Chrome_AD')
+    .pipe(crx({
+      privateKey: fs.readFileSync('./jack_Chrome_AD_crx/jack_Chrome_AD.pem', 'utf8'),
+      filename: manifest.name + '.crx'
+    }))
+    .pipe(gulp.dest('./jack_Chrome_AD_crx'))
 })
 
-crx.load("./jack_Chrome_AD")
-  .then(crx => crx.pack())
-  .then(crxBuffer => {
-    const updateXML = crx.generateUpdateXML()
-    fs.writeFile(("./update.xml"), updateXML)
-    fs.writeFile(("./jack_Chrome_AD/jack_Chrome_AD.crx"), crxBuffer)
-  })
+gulp.run('crx')
